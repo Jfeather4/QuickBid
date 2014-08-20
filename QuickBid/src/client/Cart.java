54d3b9ff3878923818e3972e95f83fba795dff6e
Jfeather4 <jfeather04@gmail.com>
2014-08-20 11:01:56 -0500

    First commit. VERY old code.

---
+++ QuickBid/src/client/Cart.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+import java.util.ArrayList;
+/**
+ *
+ * @author Jonathan
+ */
+public class Cart {
+    
+    ArrayList<Item> list;
+    
+    public Cart(){
+        list = new ArrayList();
+        
+    }
+
+    public void add(Item x){
+    list.add(x);
+    }
+    
+    public Item remove(int x){
+    Item y = list.remove(x-1);
+    return y;
+    }
+    
+    public double getTotal(){
+    double tot = 0;
+        for(Item x: list){
+            tot+=x.getTotal();
+        }
+    return tot;
+    }
+    
+    public String toString(){
+    String s = "";
+    int i = 0;
+        for(Item x: list){
+            s+= ("---- Item "+(i+1)+": ----"+x.toString());
+            i++;
+        }
+        return s;
+    }
+    public String filePrep(){
+    String s = "";
+    for(int i = 0; i < list.size(); i++)
+        if(i != list.size()-1){ // any item with "nxt" to be added
+            s+= (list.get(i).rawData()+" nxt/ ");
+        }else{
+            s+= list.get(i).rawData()+" done\\";
+        }
+        return s;
+        }
+    }
+    
+    
+
+    
+
---
+++ QuickBid/src/client/Item.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Item {
+    
+    String n, u, p;
+    static Cart ourcart = new Cart();
+   
+    
+    public Item(String name, String units, String price){
+        n = name;
+        u = units;
+        p = price;
+            
+    }
+    public double getPrice(){
+        return Double.parseDouble(p);
+    }
+    public int getUnits(){
+        return Integer.parseInt(u);
+    }
+    public String getName(){
+        return n;
+    }
+    public double getTotal(){
+        return(getPrice()*getUnits());
+    }
+    
+    public boolean isValid(){
+        try{
+            Double.parseDouble(p);
+            Integer.parseInt(u);
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+    
+    public String rawData(){
+        return (""+n+" "+u+" "+p);
+    }
+    
+    public String toString(){
+        String s = "\n"+n+": \n Price/unit: $"+p+"\n Units: "+u+"\n\n Total: \t\t$"+getTotal()+"\n--------\n";
+        return s;
+    }
+    
+    
+    
+}
